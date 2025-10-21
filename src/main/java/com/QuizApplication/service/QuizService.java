41111ea79b9150109c4928be29b973f6109aede9
tanim409 <tanimasif409@gmail.com>
2025-10-21 11:25:12 +0600

    Update QuizService.java

--- src/main/java/com/QuizApplication/service/QuizService.java
+++ src/main/java/com/QuizApplication/service/QuizService.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;      // For stream operations (optional)
+import java.util.stream.Stream;     
 @Service
 public class QuizService {
 
